f3dd602107a616484dc929ead29149b8c06a85dd
disqusag <abhinav81g@hotmail.com>
2016-12-21 02:41:01 -0600

    Percolation.java - updated PercolationStats.java - new

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,27 +1,53 @@
+/*------------------------------------------------------------
+ * Name:    Abhinav
+ * Programming Assignment 1: Percolation
+ * Dependencies: WeightedQuickUnionUF
+ * 
+ *-----------------------------------------------------------*/
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-	private int N;
-	private boolean[][] sites;
-	private int topVirtualSite;
-	private int bottomVirtualSite;
-	WeightedQuickUnionUF wquf;
+	private int N; // grid size n-by-n
+	private boolean[][] sites; // track open-blocked sites
+	private int topVirtualSite; // top virtual site
+	private int bottomVirtualSite; // bottom virtual site
+	WeightedQuickUnionUF wquf; // object for UF data structure
 	
-	public Percolation(int n) { // create n-by-n grid, with all sites blocked
+	/**
+	 * Model a percolation system using an n-by-n grid of sites, 
+	 * with all sites initially blocked.
+	 * Initialize an empty WeightedQuickUnionUF data structure with
+	 * (n*n + 2) sites.
+	 * +2 is for two virtual sites at the top and at the bottom.
+	 * 
+	 * Throws IllegalArgumentException if n {@literal <=} 0
+	 * 
+	 * @param n grid-size n-by-n where n {@literal >} 0
+	 */
+	public Percolation(int n) {
 		if (n <= 0) {
-			throw new IllegalArgumentException("N must be > 0");
+		    throw new IllegalArgumentException("N must be > 0");
 		}
 		else {
-			N = n;
-			wquf = new WeightedQuickUnionUF(n*n + 2); // +2 for top and bottom virtual sites
-			sites = new boolean[n][n]; // all sites blocked
-			topVirtualSite = 0;
-			bottomVirtualSite = N*N + 1;
-		}		 
-	}
+		    N = n;
+		    // +2 for top and bottom virtual sites
+		    wquf = new WeightedQuickUnionUF(n*n + 2);
+		    sites = new boolean[n][n];    // all sites blocked
+		    topVirtualSite = 0;
+		    bottomVirtualSite = N*N + 1;
+		    }
+    }
 	
-	public void open(int row, int col) { // open site (row, col) if it is not open already
+	/**
+	 * Open site (row, col) if it is not open already.
+	 * 
+	 * Throws IndexOutOfBoundsException if row, col not in [1, n].
+	 * 
+	 * @param row site's row-index [1, n]
+	 * @param col site's column-index [1, n]
+	 */
+	public void open(int row, int col) {
 		validateIndices(row, col);
 		int currentSiteIndex = xyTo1D(row, col);
 		if(!isOpen(row, col)) { // open the site and connect to open neighbors
@@ -53,39 +79,80 @@ public class Percolation {
 		}			
 	}
 	
+	/**
+	 * Check whether a site (row, col) is open.
+	 * 
+	 * Throws IndexOutOfBoundsException if row, col not in [1, n].
+	 * 
+	 * @param row site's row-index [1, n]
+	 * @param col site's column-index [1, n]
+	 * @return true or false
+	 */
 	public boolean isOpen(int row, int col) {
 		validateIndices(row, col);
 		return sites[row - 1][col - 1];		
 	}
 	
-	public boolean isFull (int row, int col) { // is site (row, col) full?
+	/**
+	 * Check whether a site (row, col) is full. A full site is an open site 
+	 * that can be connected to an open site in the top row via a chain of 
+	 * neighboring (left, right, up, down) open sites.
+	 * 
+	 * Throws IndexOutOfBoundsException if row, col not in [1, n].
+	 * 
+	 * @param row site's row-index [1, n]
+	 * @param col site's column-index [1, n]
+	 * @return true or false
+	 */
+	public boolean isFull (int row, int col) {
 		validateIndices(row, col);
-		if(isOpen(row, col) && wquf.connected(xyTo1D(row, col), topVirtualSite)) {
+		if(isOpen(row, col) && 
+		        wquf.connected(xyTo1D(row, col), topVirtualSite)) {
 			return true;
 		}
 		else
 			return false;		
 	}
 	
-	public boolean percolates() { // does the system percolate?
+	/**
+	 * Check whether the system percolates. The system percolates if there 
+	 * is a full site in the bottom row
+	 * @return true or false
+	 */
+	public boolean percolates() {
 		return wquf.connected(topVirtualSite, bottomVirtualSite);
-		
 	}
 	
+	/**
+	 * Map from a 2-dimensional (row, column) pair to a 1-dimensional 
+	 * union find object index
+	 * @param x site's row-index [1, n]
+	 * @param y site's column-index [1, n]
+	 * @return an integer indicating 1-d UF object index
+	 */
 	private int xyTo1D(int x, int y) {		
 		return (x - 1) * N + y;
-		
 	}
 	
+	/**
+	 * Validate row and column indices. Throw IndexOutOfBoundsException
+	 * exception if row and column indices are not between [1, n].
+	 * 
+	 * Throws IndexOutOfBoundsException if row, col not in [1, n].
+	 * 
+	 * @param row site's row-index [1, n]
+	 * @param col site's column-index [1, n]
+	 */
 	private void validateIndices(int row, int col) {
 		if (row < 1 || col < 1 || row > N || col > N) {
-			throw new IndexOutOfBoundsException("Invalid indices: row and column indices must be between 1 and " + N);
+			throw new IndexOutOfBoundsException("Invalid indices: "
+			        + "row and column indices must be between 1 and " + N);
 		}
 	}
 	
+	// test client
 	public static void main(String[] args) {
-		Percolation p = new Percolation(4);
-		
+		Percolation p = new Percolation(4);		
 		p.open(1, 1);
 		p.open(2, 2);
 		System.out.println(p.percolates());
@@ -96,7 +163,5 @@ public class Percolation {
 		p.open(2, 1);
 		System.out.println(p.percolates());
 		System.out.println(p.isFull(4, 4));
-		
-
 	}
 }
---
+++ src/PercolationStats.java
@@ -0,0 +1,111 @@
+/*------------------------------------------------------------
+ * Name:    Abhinav
+ * Programming Assignment 1: PercolationStats
+ * Dependencies: Percolation.java, StdRandom, StdStats
+ * 
+ * 
+ *-----------------------------------------------------------*/
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    
+    private int n; // grid-size n-by-n
+    private int trials; // number of experiments
+    private int openSites; // number of open sites in each experiment, 
+                           //when the system percolates
+    private int totalSites; // n*n
+    Percolation perc; // object of Percolation data type
+    private double[] fractionOfOpenSites = null; // array to store fraction 
+    // of open sites in each experiment, when the system percolates
+    
+    /**
+     * Perform trials independent experiments on an n-by-n grid
+     * @param n grid-size n-by-n where n {@literal >} 0
+     * @param trials number of computation experiments {@literal >} 0
+     */
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("grid size n and number of "
+                    + "experiments must be > 0");
+        }
+        
+        this.n = n;
+        this.trials = trials;
+        totalSites = n * n;     
+        fractionOfOpenSites = new double[trials];
+        
+        for (int t = 0; t < trials; t++) {
+            openSites = 0; // reset open sites for each experiment
+            perc = new Percolation(this.n); // create new Percolation object
+            // n*n grid of blocked sites - initially does not percolate
+            
+            while (perc.percolates() != true) {
+                //Pick a site at random and open if it is blocked
+                int i = StdRandom.uniform(1, this.n + 1);
+                int j = StdRandom.uniform(1, this.n + 1);
+                if (!perc.isOpen(i,  j)) {
+                    perc.open(i,  j);
+                    openSites++; // increment open sites counter
+                }
+            }
+            
+            // When system percolates, add fraction of open sites to the array
+            fractionOfOpenSites[t] = openSites*1.0 / totalSites;
+            /*System.out.println("Trial# " + t + " Open sites " + openSites + 
+            ", Fraction open = " + fractionOfOpenSites[t]); */
+        }
+    }
+    
+    /**
+     * Calculate sample mean of percolation threshold across all experiments
+     * @return sample mean of percolation threshold
+     */
+    public double mean() {
+        return StdStats.mean(fractionOfOpenSites);
+    }
+    
+    /**
+     * Calculate sample standard deviation of percolation threshold
+     * across all experiments.
+     * @return sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        return StdStats.stddev(fractionOfOpenSites);
+    }
+    
+    /**
+     * Calculate low endpoint of 95% confidence interval across all experiments
+     * @return low endpoint of 95% confidence interval
+     */
+	public double confidenceLo() {
+	    return (mean() - (1.96 * stddev() / Math.sqrt(this.trials)));
+	}
+
+    /**
+     * Calculate high endpoint of 95% confidence interval across all experiments
+     * @return high endpoint of 95% confidence interval
+     */	
+    public double confidenceHi() {
+        return (mean() + (1.96 * stddev() / Math.sqrt(this.trials)));
+    }	
+	
+    /**
+     * Test client to perform T independent computational experiments on an 
+     * n-by-n grid, and prints the mean, standard deviation, and the 
+     * 95% confidence interval for the percolation threshold
+     * @param args n (for n-by-n grid), T (number of experiments)
+     */
+	public static void main(String[] args) {
+ 
+	    int n = Integer.parseInt(args[0]); // n-by-n grid
+	    int T = Integer.parseInt(args[1]); // number of experiments
+	    PercolationStats ps = new PercolationStats(n, T);
+	    
+	    System.out.println("mean                       = " + ps.mean());
+	    System.out.println("stddev                     = " + ps.stddev());
+	    System.out.println("95% confidence interval    = " + 
+	    ps.confidenceLo() + ", " + ps.confidenceHi());
+	}
+}
